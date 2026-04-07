edcccd063e0a615896d3aa6611867cdb9d90d439
daiki miyanishi <miyadai0627@yahoo.co.jp>
2026-04-07 22:50:50 +0900

    feat:入力エリアを追加

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,11 @@ function App() {
   return (
     <div className="todo-app">
       <h1>TODO一覧</h1>
-      
-      
-      
+      <div className="input-area">
+        <input type="text" placeholder="タイトル" />
+        <input type="date" placeholder="yyyy/mm/dd" />
+        <button>追加</button>
+      </div>
     </div>
   )
 }
